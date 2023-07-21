f2661b07b8d6826767352ca64ef3e3edac408377
johannmanoj <johannmanoj97@gmail.com>
2023-07-21 07:16:33 +0530

    messages ui modifications

--- Client files/src/components/Chats/ChatMessages.js
+++ Client files/src/components/Chats/ChatMessages.js
@@ -19,6 +19,12 @@ const ChatMessages = () => {
 
     return (
         <div>
+          <div className='chat-message-header'>
+            <img className='chat-message-profile-pic' src={"https://www.shareicon.net/data/512x512/2016/07/05/791214_man_512x512.png"} />
+            {/* <div className='chat-message-profile-pic'></div> */}
+            <div className='chat-message-profile-name'>John</div>
+          </div>
+          
             {Messages_data.map((singleMessage)=>{
                 return(
                     <div className={singleMessage.type == "internal" ? 'chat-message-internal-align' : 'chat-message-external-align'}>
