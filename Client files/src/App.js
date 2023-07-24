bc6e5b189217dc1769aa02b945405aca0b89c3f0
johannmanoj <johannmanoj97@gmail.com>
2023-07-24 16:06:57 +0530

    login page

--- Client files/src/App.js
+++ Client files/src/App.js
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import MainPage from './components/MainPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
   const [main_page, setMain_page] = useState("all_chats")
   console.log("main_page",main_page);
   return (
     <div className="App">
-      <Sidebar setMain_page_func = {setMain_page} />
-      <MainPage mainPageData = {main_page}/>
+      <LoginPage />
+      {/* <Sidebar setMain_page_func = {setMain_page} />
+      <MainPage mainPageData = {main_page}/> */}
     </div>
   );
 }
---
+++ Client files/src/components/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LoginPage.css'
+
+const LoginPage = () => {
+
+
+  return (
+    <div className='login-background'>
+        <div className='login-card'>
+            LoginPage
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
