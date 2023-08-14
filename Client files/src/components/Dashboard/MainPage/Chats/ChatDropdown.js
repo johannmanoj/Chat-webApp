60f132e86f0d1c2dfc52c5ea9280a3ef26ebad8f
johannmanoj <johannmanoj97@gmail.com>
2023-08-14 18:36:37 +0530

    sidebar changes

--- Client files/src/components/Dashboard/MainPage/Chats/ChatDropdown.js
+++ Client files/src/components/Dashboard/MainPage/Chats/ChatDropdown.js
@@ -9,9 +9,8 @@ const ChatDropdown = (props) => {
             <div className='chat-dropdown-backdrop' onClick= {() => props.setDropdownVisibility(false)}/>
 
             <div className='chat-dropdown-modal'>
-                <div>Option 1</div>
-                <div>Option 2</div>
-                <div>Option 3</div>
+                <div>Add Contact</div>
+                <div>Settings</div>
             </div>
         </div>
     )
