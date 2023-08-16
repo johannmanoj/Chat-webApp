e1a17edc7a5929f2dc7cee5c8baf5018c931623f
johannmanoj <johannmanoj97@gmail.com>
2023-08-16 16:25:18 +0530

    add new contact ui changes

--- Client files/src/components/Dashboard/MainPage/Chats/AddContactDropdown.js
+++ Client files/src/components/Dashboard/MainPage/Chats/AddContactDropdown.js
@@ -2,77 +2,86 @@ import "./AddContactDropdown.css";
 import { FaDoorOpen } from "react-icons/fa";
 import axios from "axios";
 import { useState } from "react";
-import { FaSearch} from "react-icons/fa";
+import { FaSearch, FaPlus} from "react-icons/fa";
 
 const AddContactDropdown = (props) => {
-    const [contactSearch, setContactSearch] = useState([])
-    const [searchName, setSearchName] = useState("")
+  const [contactSearch, setContactSearch] = useState([])
+  const [searchName, setSearchName] = useState("")
 
-    const [searchShow, setSearchShow] = useState(false)
+  const [searchShow, setSearchShow] = useState(false)
 
 
 
-    const nameChangeHandler = (event) =>{ setSearchName(event.target.value) }
+  const nameChangeHandler = (event) =>{ setSearchName(event.target.value) }
 
-    const add_contact = async () =>{
-        const config = {
-          method: 'post',
-          url: `http://localhost:8080/log-contact`,
-          data : {"user" :localStorage.getItem("userEmail"), "email":contactSearch[0]["email"]}
-        }
-        await axios(config)
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.log(error.response);
-          });
+  const add_contact = async () =>{
+      props.setAddContactDropdownVisibility(false)
+      const config = {
+        method: 'post',
+        url: `http://localhost:8080/log-contact`,
+        data : {"user" :localStorage.getItem("userEmail"), "email":contactSearch[0]["email"]}
+      }
+      await axios(config)
+        .then((response) => {
+          console.log(response.data);
+          // props.setAddContactDropdownVisibility(false)
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+  }
+
+  // console.log("contactSearch------",contactSearch);
+
+  const submitHandler = async (event) =>{
+    event.preventDefault()
+    const config = {
+        method: 'post',
+        url: `http://localhost:8080/email-search`,
+        data : {"email" :searchName}
     }
+    await axios(config)
+      .then((response) => {
+        setSearchShow(true)
+        setContactSearch(response.data)
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
+  
+  return(
+    <div>
+        <div className='chat-dropdown-backdrop' onClick= {() => props.setAddContactDropdownVisibility(false)}/>
 
-    // console.log("contactSearch------",contactSearch);
+        <div className='add-contact-modal'>
+          <div className="add-contact-header">
+              <input className='add-contact-search' onChange={nameChangeHandler}></input>
+              <FaSearch className="add-contact-search-icon" onClick={submitHandler}/>
+          </div>
+            
+            
+      
+          <div className="add-contact-search-result">
+              {searchShow && 
+                (contactSearch.length > 0 ? 
+                  <div className="add-contact-search-result-item">
+                    <div className= "add-contact-search-result-item-name">{contactSearch[0]["email"]}</div>
+                    { contactSearch.length > 0 && <FaPlus className="add-contact-plus-icon" onClick={() => add_contact()} />}
+                  </div> 
+                  : 
+                  <div className="add-contact-search-result-empty">
+                    <div> No matching contact found </div>
+                  </div>
+                )
+              }
 
-    const submitHandler = async (event) =>{
-        event.preventDefault()
-        const config = {
-            method: 'post',
-            url: `http://localhost:8080/email-search`,
-            data : {"email" :searchName}
-          }
-          await axios(config)
-            .then((response) => {
-              setSearchShow(true)
-              setContactSearch(response.data)
-            })
-            .catch((error) => {
-              console.log(error.response);
-            });
-    }
-
-    
-
-    
-    return(
-        <div>
-            <div className='chat-dropdown-backdrop' onClick= {() => props.setAddContactDropdownVisibility(false)}/>
-
-            <div className='add-contact-modal'>
-                    <div className="add-contact-header">
-                        <input className='add-contact-search' onChange={nameChangeHandler}></input>
-                        <FaSearch className="add-contact-search-icon" onClick={submitHandler}/>
-                    </div>
-                
-                
-          
-                <div className="add-contact-search-result">
-                    {searchShow && <div className="add-contact-search-result-item">
-                        <div className="add-contact-search-result-item-name">{contactSearch.length > 0 ? contactSearch[0]["email"] : "No contact found"}</div>
-                        { contactSearch.length > 0 && <button onClick={() => add_contact()}>Add</button>}
-                    </div>}
-                    {!searchShow && "Search"}
-                </div>
-            </div>
+              
+              {!searchShow && "Search"}
+          </div>
         </div>
-    )
+    </div>
+  )
 };
 
 
